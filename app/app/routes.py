cdb399896ad9998187d4e2571de50ed2cc0e9db7
airladon <airladon@gmail.com>
2019-09-18 09:46:03 -0700

    working 50k files

--- app/app/routes.py
+++ app/app/routes.py
@@ -6,6 +6,8 @@ import boto3
 from datetime import datetime
 from datetime import timezone
 
+log_file = '/opt/app/log.txt'
+
 
 def write_file(area, file_path, file_name):
     log_storage_address = os.environ.get('LOG_STORAGE_ADDRESS')
@@ -22,8 +24,7 @@ def write_file(area, file_path, file_name):
         aws_access_key_id=log_storage_access_key,
         aws_secret_access_key=log_storage_secret,
     )
-    file_path_name = f'{file_path}/{file_name}'
-    client.upload_file(file_path_name, area, file_name)
+    client.upload_file(log_file, area, file_name)
 
 
 @app.route('/')
@@ -32,13 +33,20 @@ def home():
     return jsonify({'status': 'ok'})
 
 
+def add_to_log(path):
+    with open(log_file, 'a+') as f:
+        f.write(request.data.decode('utf-8'))
+    if os.path.isfile(log_file) and os.path.getsize(log_file) > 50000:
+        write_to_space(path)
+        with open(log_file, 'w') as f:
+            f.write('')
+
+
 def write_to_space(path):
     now = datetime.now(timezone.utc)
     dt_string = now.strftime("%Y.%m.%d-%H.%M.%S.%f")
     file_name = f'{dt_string}.txt'
-    with open(file_name, 'w') as f:
-        f.write(request.data.decode('utf-8'))
-    write_file(path, '/opt/app', file_name)
+    write_file('dev', '/opt/app', file_name)
 
 
 @auth.verify_password
@@ -56,19 +64,19 @@ def verify_password(username, password):
 @app.route('/dev', methods=['GET', 'POST'])
 @auth.login_required
 def log_dev():
-    write_to_space('dev')
+    add_to_log('dev')
     return ''
 
 
 @app.route('/test', methods=['GET', 'POST'])
 @auth.login_required
 def log_test():
-    write_to_space('dev')
+    add_to_log('dev')
     return ''
 
 
 @app.route('/log', methods=['GET', 'POST'])
 @auth.login_required
 def log_prod():
-    write_to_space('dev')
+    add_to_log('dev')
     return ''
